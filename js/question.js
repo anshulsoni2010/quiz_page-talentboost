bfd9142224b0087535b899a815217fd92784900a
Anshul Soni <102960277+anshulsoni2010@users.noreply.github.com>
2023-04-04 12:00:38 +0000

    all done

--- js/question.js
+++ js/question.js
@@ -16,52 +16,42 @@ const quiz = [
 	{
 		q: 'Which HTML tag is used to define a table?',
 		options: ['<tr>','<table>','<td>','<th>'],
-		answer: 2
-	},
-	{
-		q: 'What does CSS stand for?',
-		options: ['Complete Sound Sheet', 'Cascading Style Sheet', 'Cscading Style Syntax'],
 		answer: 1
 	},
 	{
-		q: 'Non animated gifs are usually lower in file size, why don\'t people use them for photos',
-		options: ["Because it hasn't caught on.", 'You need to pay for a lisencse to use them.', 'They are distracting and decrease Usability.', 'None of the above'],
+		q: 'What is the purpose of the header tag in HTML?',
+		options: ['The header tag in HTML is used to define the body content of a document or section.', 'The header tag in HTML is used to define a footer section for a document or section.', 'The header tag in HTML is used to define a header section for a document or section, which typically contains a title, logo, navigation menu, or other important information.'],
 		answer: 2
 	},
 	{
-		q: "What happens to a lot of images saved as jpgs when you put them online.",
-		options: ['Stretching', "They'll turn blur", 'They are too big in file size.', 'Lower quality and artifacts.'],
+		q: 'Which of the following is the purpose of the header tag in HTML?',
+		options: ["To define a footer section for a document or section.", 'To insert an image in a document.', 'To create a list of items.', 'To define a header section for a document or section, which typically contains a title, logo, navigation menu, or other important information.'],
 		answer: 3
 	},
 	{
-		q: "Which is the correct CSS syntax?",
-		options: ["Body {color: black}", "{body;color:black}", "{body:color=black(body}", "body:color=black"],
-		answer: 0
-	},
-	{
-		q: "What does a compiler do?   ",
-		options: ["Makes code start", "Tests to see whether the program runs fine", "Converts the coding into another computer language, usually to make an executable program."],
+		q: "What is a the correct syntax for an HTML5 documentation?",
+		options: ['<!DOCTYPE HTML5>', "<html5>>", '<!DOCTYPE html>', 'Non of the above'],
 		answer: 2
 	},
 	{
-		q: "What is an advantage of having external CSS?",
-		options: ["It is faster", "You can edit one file to edit the base of every page which uses it.", "It makes no difference"],
+		q: "Which of the following is NOT a valid HTML5 tag?",
+		options: ["<article>", "<aside>", "<dialog>", "<section/>"],
+		answer: 2
+	},
+	{
+		q: "Which of the following attributes specifies the target for a link? ",
+		options: ["srct", "href", "target","link"],
+		answer: 2
+	},
+	{
+		q: "Which of the following is the correct syntax for an external JavaScript file?",
+		options: ['<script href="myscript.js"></script>', '<script type="text/javascript" src="myscript.js"></script>', '<script file="myscript.js"></script>','<script src="myscript.js"></script>'],
+		answer: 3
+	},
+	{
+		q: " Which of the following tags is used to define a table header cell?",
+		options: ["<td>", "<th>", "<tr>", "<table-head"],
 		answer: 1
-	},
-	{
-		q: "What does the acronym WWW stand for?",
-		options: ["Wide Wacky Walter", "Web Wide Wave", "Wide Wall Web", "Web World Wall", "World Wide Web"],
-		answer: 4
-	},
-	{
-		q: "HTML uses______?",
-		options: ["User defined tags", "Pre-specified tags", "Fixed tags defined by the language", "Tags only for linking"],
-		answer: 2
-	},
-	{
-		q: "The year in which HTML was first proposed _______?",
-		options: ["1993", "2001", "1951", "1890"],
-		answer: 0
 	}
 ]
 
